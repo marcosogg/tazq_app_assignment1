a885ccb6ad6900ee3e66dddfc98c9bcc7d4d4242
Marcos Oliveira Gomes <115948731+marcosogg@users.noreply.github.com>
2024-10-28 20:00:21 +0000

    feat(ui): add splash screen with app logo

---
+++ app/src/main/java/org/wit/tazq_app/SplashActivity.kt
@@ -0,0 +1,24 @@
+package org.wit.tazq_app
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 2000 // 2 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Start the main activity
+            val intent = Intent(this@SplashActivity, TaskActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeOut)
+    }
+}
